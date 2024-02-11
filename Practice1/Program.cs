4a8a084c0ceee2108d6297d177d943779cba0cf8
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2024-02-11 21:57:44 +0200

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter any number");
+string input = Console.ReadLine();
+
+int muk = int.Parse(input);
+
+Console.Write("You have entered " + muk);
+
