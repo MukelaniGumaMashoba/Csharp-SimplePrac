bf6e9d598f8b1fbef7ece6bc1185d8aadca94adf
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2024-02-11 23:42:10 +0200

    added a class and function

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
+﻿//Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
 //string input = Console.ReadLine();
 //int muk = int.Parse(input);
 //Console.WriteLine("wat is jou naam: ");
@@ -11,33 +10,41 @@
 //}
 //else
 //{
- //   Console.Write("Your ouderdom and naam is .... " + muk + " " + nam);
+//   Console.Write("Your ouderdom and naam is .... " + muk + " " + nam);
 //}
 
-int count = 3; //you have only 3 attemps...
-for(int i = 0; i < count; i--)
+internal class Program
 {
-    Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
-    string input = Console.ReadLine();
-    int muk = int.Parse(input);
-    Console.WriteLine("wat is jou naam: ");
-    string nam = Console.ReadLine();
+    private static void Main(string[] args)
+    {
+        int count = 3; //you have only 3 attemps...
+        for (int i = 0; i < count; i--)
+        {
+            Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
+            string input = Console.ReadLine();
+            int muk = int.Parse(input);
+            Console.WriteLine("wat is jou naam: ");
+            string nam = Console.ReadLine();
 
-    if (nam == "mukelani" && muk == 20)
-    {
-        Console.Write("You entered the correct credintials ");
-        break;
-    }
-    else if (count == 0)
-    {
-        break;
-    }
-    else
-    {
-        Console.Write("Sorry Wrong Password.....Try again");
-        count -= 1;
-        continue;
-        
-    }
+            if (nam == "mukelani" && muk == 20)
+            {
+                Console.Write("You entered the correct credintials ");
+                break;
+            }
+            else if (count == 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.Write("Sorry Wrong Password.....Try again");
+                count -= 1;
+                continue;
 
+            }
+
+
+
+        }
+    }
 }
