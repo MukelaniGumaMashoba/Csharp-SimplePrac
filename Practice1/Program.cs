3d29e3f35036801bd3499da539b507f950b614d9
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2024-02-11 22:43:14 +0200

    working 3 attempts

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,16 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
-string input = Console.ReadLine();
-int muk = int.Parse(input);
-Console.WriteLine("wat is jou naam: ");
-string nam = Console.ReadLine();
+//Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
+//string input = Console.ReadLine();
+//int muk = int.Parse(input);
+//Console.WriteLine("wat is jou naam: ");
+//string nam = Console.ReadLine();
 
-if(nam != "mukelani")
-{
-    Console.Write("Wrong naam sorry...");
-}
-else
-{
-    Console.Write("Your ouderdom and naam is .... " + muk + " " + nam);
-}
+//if(nam != "mukelani")
+//{
+//    Console.Write("Wrong naam sorry...");
+//}
+//else
+//{
+ //   Console.Write("Your ouderdom and naam is .... " + muk + " " + nam);
+//}
 
+int count = 3; //you have only 3 attemps...
+for(int i = 0; i < count; i--)
+{
+    Console.WriteLine("Please enter age/ wat is jou ouderdom ? ");
+    string input = Console.ReadLine();
+    int muk = int.Parse(input);
+    Console.WriteLine("wat is jou naam: ");
+    string nam = Console.ReadLine();
+
+    if (nam == "mukelani" && muk == 20)
+    {
+        Console.Write("You entered the correct credintials ");
+        break;
+    }
+    else if (count == 0)
+    {
+        break;
+    }
+    else
+    {
+        Console.Write("Sorry Wrong Password.....Try again");
+        count -= 1;
+        continue;
+        
+    }
+
+}
